10d0a7cd3cd3d7d2e8e9c19b3819cbd37ea8c694
Sebastian Helzle <sebastian@helzle.it>
2025-11-14 09:09:18 +0100

    FEATURE: Support Neos 9.1 ckeditor toolbar

--- Resources/Private/Scripts/HyphensEditor/src/plugins/hyphens.js
+++ Resources/Private/Scripts/HyphensEditor/src/plugins/hyphens.js
@@ -1,7 +1,10 @@
-import { ViewRange } from 'ckeditor5-exports';
+// Neos.Ui 8.3 -> 9.0 compatible import
+import { ViewRange, ButtonView } from 'ckeditor5-exports';
+
 import ShyCommand from '../commands/shy';
 import NbspCommand from '../commands/nbsp';
 import './hyphens.css';
+import { CKEDITOR_TOOLBAR_ENABLED } from '../manifest.config';
 
 const softHyphenCharacter = '\u00AD';
 const nbspCharacter = '\u00A0';
@@ -13,6 +16,46 @@ function HyphensFactory(config) {
 
         if (config.shortcut) {
             editor.keystrokes.set(config.shortcut, 'insertShyEntity');
+        }
+
+        // Add buttons to ckeditor toolbar for CKEditor version 47 and higher which is enabled in Neos 9.1+
+        if (CKEDITOR_TOOLBAR_ENABLED) {
+            editor.ui.componentFactory.add('hyphens', () => {
+                const buttonView = new ButtonView();
+
+                buttonView.set({
+                    label: 'Insert soft hyphen',
+                    withText: false,
+                    tooltip: true,
+                    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="18.109" height="9.697" viewBox="0, 0, 18.109, 9.697"><g stroke="currentColor" stroke-miterlimit="3.864" fill="none"><path d="M2.596 1a5.44 5.44 0 0 0 0 7.697m12.918 0a5.44 5.44 0 0 0 0-7.697" stroke-width=".907"></path><path d="M4.52 4.848h9.07" stroke-width="1.814"></path></g></svg>',
+                });
+
+                // Execute the command when button is clicked
+                buttonView.on('execute', () => {
+                    editor.execute('insertShyEntity');
+                    editor.editing.view.focus();
+                });
+
+                return buttonView;
+            });
+            editor.ui.componentFactory.add('nbsp', () => {
+                const buttonView = new ButtonView();
+
+                buttonView.set({
+                    label: 'Insert non-breaking space',
+                    withText: false,
+                    tooltip: true,
+                    icon: '<svg width="18px" height="9px" viewBox="0 0 18 9" version="1.1" xmlns="http://www.w3.org/2000/svg"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square"><path d="M16.3846154,2.61538462 L16.3846154,7.53846154 M16.3846154,7.53846154 L1.61538462,7.53846154 M1.61538462,2.61538462 L1.61538462,7.53846154" id="Combined-Shape" stroke="currentColor" stroke-width="1.8"></path></g></svg>',
+                });
+
+                // Execute the command when button is clicked
+                buttonView.on('execute', () => {
+                    editor.execute('insertNbspEntity');
+                    editor.editing.view.focus();
+                });
+
+                return buttonView;
+            });
         }
 
         editor.conversion.for('editingDowncast').add((dispatcher) => {
